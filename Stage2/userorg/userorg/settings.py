2f9b985fe76876b60b55f06fb17a776f6795a236
Samilincoln <agboolaola150@gmail.com>
2024-07-08 15:24:03 +0100

    updated settings for json interface

--- Stage2/userorg/userorg/settings.py
+++ Stage2/userorg/userorg/settings.py
@@ -149,6 +149,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+
 }
 
 SIMPLE_JWT = {
